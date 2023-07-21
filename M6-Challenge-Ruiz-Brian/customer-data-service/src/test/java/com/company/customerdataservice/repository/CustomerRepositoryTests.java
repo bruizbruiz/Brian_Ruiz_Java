c37398acbe304d3a8a8e91e669586eaa4191c6f6
Brian Ruiz <98550010+bruizbruiz@users.noreply.github.com>
2023-07-20 21:17:14 -0400

    added more tests

--- M6-Challenge-Ruiz-Brian/customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTests.java
+++ M6-Challenge-Ruiz-Brian/customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -90,6 +91,68 @@ public class CustomerRepositoryTests {
 
     @Test
     public void getCustomerById() {
-        
+        Customer customer = new Customer();
+        customer.setFirstName("Brian");
+        customer.setLastName("Ruiz");
+        customer.setAddress1("225 Chestnut St");
+        customer.setCity("NWR");
+        customer.setState("NJ");
+        customer.setCountry("United States");
+        customer.setPostalCode("07102");
+        customer.setCompany("Pepsi");
+        customer.setEmail("bruiz@pepsi.co");
+        customer.setPhone("111-223-7584");
+
+        customer = repo.save(customer);
+
+        Customer customer1 = new Customer();
+        customer1.setFirstName("Joe");
+        customer1.setLastName("Billy");
+        customer1.setAddress1("123 Place St");
+        customer1.setCity("Union");
+        customer1.setState("NY");
+        customer1.setCountry("United States");
+        customer1.setPostalCode("07087");
+        customer1.setCompany("Coca Cola");
+        customer1.setEmail("BillyJoe@cocacola.com");
+        customer1.setPhone("246-421-5342");
+
+        customer1 = repo.save(customer);
+
+        Optional<Customer> foundCustomer = repo.findById(customer.getId());
+        assertEquals(foundCustomer.get(), customer);
+    }
+
+    @Test
+    public void getCustomerByState() {
+        Customer customer = new Customer();
+        customer.setFirstName("Brian");
+        customer.setLastName("Ruiz");
+        customer.setAddress1("225 Chestnut St");
+        customer.setCity("NWR");
+        customer.setState("NJ");
+        customer.setCountry("United States");
+        customer.setPostalCode("07102");
+        customer.setCompany("Pepsi");
+        customer.setEmail("bruiz@pepsi.co");
+        customer.setPhone("111-223-7584");
+
+        customer = repo.save(customer);
+
+        Customer customer1 = new Customer();
+        customer1.setFirstName("Joe");
+        customer1.setLastName("Billy");
+        customer1.setAddress1("123 Place St");
+        customer1.setCity("Union");
+        customer1.setState("NY");
+        customer1.setCountry("United States");
+        customer1.setPostalCode("07087");
+        customer1.setCompany("Coca Cola");
+        customer1.setEmail("BillyJoe@cocacola.com");
+        customer1.setPhone("246-421-5342");
+
+        customer1 = repo.save(customer);
+        List<Customer> customers = repo.findCustomerByState("NJ");
+        assertEquals(customers.size(), 1);
     }
 }
