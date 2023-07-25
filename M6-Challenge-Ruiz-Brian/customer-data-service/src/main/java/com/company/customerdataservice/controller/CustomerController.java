38926265be25128820510a58bed0e37debdb6506
Brian Ruiz <98550010+bruizbruiz@users.noreply.github.com>
2023-07-24 20:52:01 -0400

    created test database

---
+++ M6-Challenge-Ruiz-Brian/customer-data-service/src/main/java/com/company/customerdataservice/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package com.company.customerdataservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+    // CRUD routes
+    // For CustomerControllerTest create MockMVC object to test Http Status
+}
--- M6-Challenge-Ruiz-Brian/customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTests.java
+++ M6-Challenge-Ruiz-Brian/customer-data-service/src/test/java/com/company/customerdataservice/repository/CustomerRepositoryTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
