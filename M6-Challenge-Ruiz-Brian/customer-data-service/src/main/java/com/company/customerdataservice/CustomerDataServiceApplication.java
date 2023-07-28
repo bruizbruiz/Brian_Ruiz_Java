1940fe77952a7ccc24c14b582b3f91315cc90fae
Brian Ruiz <98550010+bruizbruiz@users.noreply.github.com>
2023-07-27 20:56:21 -0400

    added yaml file

--- M6-Challenge-Ruiz-Brian/customer-data-service/src/main/java/com/company/customerdataservice/CustomerDataServiceApplication.java
+++ M6-Challenge-Ruiz-Brian/customer-data-service/src/main/java/com/company/customerdataservice/CustomerDataServiceApplication.java
@@ -3,7 +3,7 @@ package com.company.customerdataservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"controller", "model", "repository"})
+@SpringBootApplication
 public class CustomerDataServiceApplication {
 
 	public static void main(String[] args) {
